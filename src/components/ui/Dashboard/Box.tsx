ecccbb08fce3a9531f1f7458fc07fbdd0e512b15
joshuaOyewole <joshpp2013@gmail.com>
2023-04-04 11:50:06 +0100

    added MultiLayout Box and modified EOD

--- src/components/ui/Dashboard/Box.tsx
+++ src/components/ui/Dashboard/Box.tsx
@@ -1,21 +1,22 @@
 type BoxProps = {
-icon: React.ReactElement,
-title: string,
-value?: number | string
-}
+  icon?: React.ReactElement;
+  title: string;
+  value?: number | string;
+  hClass?: string;
+  pClass?: string;
+  boxStyle?: string;
+};
 
-const Box = (props:BoxProps) => {
-    const {icon, title,value} = props;
+const Box = (props: BoxProps) => {
+  const { boxStyle, icon, title, value, hClass, pClass } = props;
 
-    return (
-        <div className="dashboard__box">
-            <div className="dashboard__icon-container">
-                {icon}
-            </div>
-            <h2 className="dashboard__content--heading">{value}</h2>
-            <h4>{title}</h4>
-        </div>
-    )
-}
+  return (
+    <div className={`dashboard__box ${boxStyle}`}>
+      {icon && <div className="dashboard__icon-container">{icon}</div>}
+      <h2 className={`dashboard__content--heading ${hClass}`}> {value}</h2>
+      <h4 className={`${pClass}`}>{title}</h4>
+    </div>
+  );
+};
 
-export default Box
+export default Box;
