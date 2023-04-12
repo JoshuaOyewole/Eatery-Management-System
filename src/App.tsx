a457452b941c26d9394d3f5fee47ff5d0843a869
joshuaOyewole <joshpp2013@gmail.com>
2023-04-12 18:36:21 +0100

    added React-Auth-Kit Token

--- src/App.tsx
+++ src/App.tsx
@@ -14,27 +14,112 @@ import Error from "./pages/404/Index";
 import OrderMeal from "./pages/OrderMeal/Index";
 import OrderDetail from "./pages/ViewRecords/Order";
 import ViewOrderLayout from "./pages/ViewRecords/viewOrderLayout";
-import Profile from "./pages/Profile/Index"
+import Profile from "./pages/Profile/Index";
+import { RequireAuth } from "react-auth-kit";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/admin/register" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={
+          <RequireAuth loginPath="/">
+            <Dashboard />
+          </RequireAuth>
+        }
+      />
       <Route path="/eod" element={<EOD />} />
-      <Route path="/eod/filter" element={<EODDate />} />
-      <Route path="/records" element={<ViewOrderLayout />}>
-        <Route path="orders/:orderId" element={<OrderDetail />} />
-        <Route path="orders" element={<ViewRecord />} />
-        <Route path="orders/search?date=eodDate" element={<ViewRecord />} />
+      <Route
+        path="/eod/filter"
+        element={
+          <RequireAuth loginPath="/">
+            <EODDate />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/records"
+        element={
+          <RequireAuth loginPath="/">
+            <ViewOrderLayout />
+          </RequireAuth>
+        }
+      >
+        <Route
+          path="orders/:orderId"
+          element={
+            <RequireAuth loginPath="/">
+              <OrderDetail />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="orders"
+          element={
+            <RequireAuth loginPath="/">
+              <ViewRecord />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="orders/search?date=eodDate"
+          element={
+            <RequireAuth loginPath="/">
+              <ViewRecord />
+            </RequireAuth>
+          }
+        />
       </Route>
-      <Route path="/order-meal" element={<OrderMeal />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/manage-orders" element={<ManageOrders />} />
-      <Route path="/printReceipt/:id" element={<Receipt />} />
-      <Route path="/printEODSummary" element={<EODSummary />} /> 
-      <Route path="/printEODReport" element={<EODReport />} /> 
+      <Route
+        path="/order-meal"
+        element={
+          <RequireAuth loginPath="/">
+            <OrderMeal />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <RequireAuth loginPath="/">
+            <Profile />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/manage-orders"
+        element={
+          <RequireAuth loginPath="/">
+            <ManageOrders />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/printReceipt/:id"
+        element={
+          <RequireAuth loginPath="/">
+            <Receipt />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/printEODSummary"
+        element={
+          <RequireAuth loginPath="/">
+            <EODSummary />
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/printEODReport"
+        element={
+          <RequireAuth loginPath="/">
+            <EODReport />
+          </RequireAuth>
+        }
+      />
       <Route path="*" element={<Error />} />
     </Routes>
   );
