774b2db94c59a829b87aebec061a8b879357a77e
joshuaOyewole <joshpp2013@gmail.com>
2023-04-12 18:37:55 +0100

    added React Authentication (Login, Register) using React-Auth-Kit

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './sass/main.scss'
 import App from './App';
+import {AuthProvider} from "react-auth-kit";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
+  <AuthProvider
+  authType={'cookie'}
+  authName={"_auth"}
+  cookieDomain={window.location.hostname}
+  cookieSecure={false}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </AuthProvider>
+
 
 );
 
