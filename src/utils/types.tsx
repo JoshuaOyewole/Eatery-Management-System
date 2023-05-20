b05c92d61e47db8173fdacd227644fac8eff9765
joshuaOyewole <joshpp2013@gmail.com>
2023-05-20 17:11:54 +0100

    created utilities types

---
+++ src/utils/types.tsx
@@ -0,0 +1,19 @@
+ //Staff props
+ export type StaffProps = {
+    firstname: string,
+    lastname: string,
+    email: string,
+    password: string,
+    phone: string,
+    gender: string,
+    homeAddress: string,
+    dob: string,
+    state: string,
+    lga: string,
+    passport: string,
+}
+
+export type loginCredentialsProps = {
+    email:string,
+    password:string
+}
