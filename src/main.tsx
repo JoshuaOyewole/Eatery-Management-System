441915b9826ea67c48864511ceff807884a84d8e
joshuaOyewole <joshpp2013@gmail.com>
2023-05-24 23:36:32 +0100

    added redux persisit codes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 
 import ReactDOM from 'react-dom/client';
 import './sass/main.scss'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App';
 import { Provider } from 'react-redux';
 //import { AuthProvider } from 'react-auth-kit';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/store';
+
+
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -14,6 +19,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+      </PersistGate>
       <App />
     </Provider>
   </BrowserRouter>
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,16 +1,43 @@
-import { configureStore} from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import authReducer from "./features/auth/authSlice";
 import staffReducer from "./features/staffs/staffSlice";
 import userReducer from "./features/users/userSlice";
+import mealReducer from "./features/meal/mealSlice";
 
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+  
+  const rootReducer = combineReducers({
+    user: userReducer,
+    staff: staffReducer,
+    auth: authReducer,
+    meal: mealReducer
+})
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = configureStore({
-    reducer: {
-        user: userReducer,
-        staff: staffReducer,
-        auth:authReducer
-    }
-  });
- export default store;
- export type RootState = ReturnType<typeof store.getState>
- export type AppDispatch = typeof store.dispatch
+    reducer:persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export default store;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
