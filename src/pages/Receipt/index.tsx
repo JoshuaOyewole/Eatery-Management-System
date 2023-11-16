1711f61a10ebf58fd9bd0d9058e6a9a30549b8db
joshua Oyewole <joshpp2013@gmail.com>
2023-11-16 23:54:39 +0100

    resolved unable to print receipt bug

--- src/pages/Receipt/index.tsx
+++ src/pages/Receipt/index.tsx
@@ -1,8 +1,9 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Styles from "./_receipt.module.scss";
+import ReactLoading from "react-loading";
 import axios from "axios";
-import { OrderProps } from "../../../types";
+import { OrderProps } from "../../utils/types"
 
 
 /* 
@@ -12,16 +13,11 @@ You can bring a loading component and hide only when the data queried is availab
 */
 function Index() {
   const navigate = useNavigate();
-  const { id } = useParams();//Order ID
+  let { id } = useParams();//Order ID
+  const [isLoading, setIsLoading] = useState<boolean>(false)
   const [orderInfo, setOrderInfo] = useState<OrderProps | null>(null);
 
-  const fetchReceiptInfo = async () => {
-    const response = await axios.get(`https://eatman-api.onrender.com/api/order/${id}`,
-    {
-      headers: { "Authorization": `Bearer ${localStorage.getItem('token')}` } 
-    });
-    setOrderInfo(response?.data);
-  };
+
 
   function printWindow() {
     window.print();
@@ -29,96 +25,116 @@ function Index() {
   }
 
   useEffect(() => {
-    fetchReceiptInfo();
-    //Delay of 50ms was added so the response from the promise can be gotten and the UI updated with the correct details before printing
-    const interval = setTimeout(() => printWindow(), 50);
 
-    return () => {
-      clearTimeout(interval);
+    const fetchReceiptInfo = async (id: string | undefined) => {
+      setIsLoading(true)
+      const response = await axios.get(`https://eatman-api.onrender.com/api/order/${id}`,
+        {
+          headers: { "Authorization": `Bearer ${localStorage.getItem('token')}` }
+        });
+      const res = await response.data;
+      setOrderInfo(res);
+      setIsLoading(false)
     };
+    fetchReceiptInfo(id);
+
   }, []);
 
-  return (
-    <div className={Styles["invoice-POS"]}>
-      <div className={Styles.top}>
-        <div className={Styles.logo}></div>
-        <div className={Styles.info}>
-          <h2 className={Styles.legal}>Orisfina Eatery</h2>
-          {/* <p className={Styles.address}>
-            Motto : Customer's Satisfactory is our priority{" "}
-          </p> */}
-          <p className={Styles.address}>Address : Angle 90, Auchi Edo state </p>
-          <p className={Styles.address}>Email : info@orisfinaeatery.com </p>
-        </div>
-      </div>
+  useEffect(() => {
+    if (orderInfo !== null) {
+      printWindow();
+    }
+  }, [orderInfo])
 
-      <div className={Styles.mid}>
-        <div className={Styles.info}>
-          <h3>Payment Receipt</h3>
-        </div>
-      </div>
 
-      <div className={Styles.bot}>
-        <div className={Styles.table}>
-          <table>
-            <>
-              <thead>
-                <tr className={Styles.tabletitle}>
-                  <td className={Styles.item}>
-                    <h2>Item</h2>
-                  </td>
-                  <td className={Styles.Hours}>
-                    <h2>Qty</h2>
-                  </td>
-                  <td className={Styles.Rate}>
-                    <h2>Sub Total</h2>
-                  </td>
-                </tr>
-              </thead>
-              <tbody>
-                <>
-                  {orderInfo?.orders.map((item, index) => {
-                    return (
-                      <tr className={Styles.service} key={index}>
-                        <td className={Styles.tableitem}>
-                          <p className={Styles.itemtext}> {item.meal}</p>
-                        </td>
-                        <td className={Styles.tableitem}>
-                          <p className={Styles.itemtext}>{item.quantity}</p>
-                        </td>
-                        <td className={Styles.tableitem}>
-                          <p className={Styles.itemtext}>{item.totalAmount}</p>
-                        </td>
-                      </tr>
-                    );
-                  })}
+  {
+    if (isLoading) {
+      return <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}><ReactLoading type={"bars"} color="#333" /></div>
+    }
+    else {
+      return <div className={Styles["invoice-POS"]}>
+        <div className={Styles.top}>
+          <div className={Styles.logo}></div>
+          <div className={Styles.info}>
+            <h2 className={Styles.legal}>Orisfina Eatery</h2>
+            {/* <p className={Styles.address}>
+      Motto : Customer's Satisfactory is our priority{" "}
+    </p> */}
+            <p className={Styles.address}>Address : Angle 90, Auchi Edo state </p>
+            <p className={Styles.address}>Email : info@orisfinaeatery.com </p>
+          </div>
+        </div>
+
+        <div className={Styles.mid}>
+          <div className={Styles.info}>
+            <h3>Payment Receipt</h3>
+          </div>
+        </div>
+
+        <div className={Styles.bot}>
+          <div className={Styles.table}>
+            <table>
+              <>
+                <thead>
                   <tr className={Styles.tabletitle}>
-                    <td></td>
-                    <td className={Styles.Rate}>
-                      <h2>Total</h2>
+                    <td className={Styles.item}>
+                      <h2>Item</h2>
                     </td>
-                    <td className={Styles.payment}>
-                      <h2>{orderInfo?.totalPrice}</h2>
+                    <td className={Styles.Hours}>
+                      <h2>Qty</h2>
+                    </td>
+                    <td className={Styles.Rate}>
+                      <h2>Sub Total</h2>
                     </td>
                   </tr>
-                </>
-              </tbody>
-            </>
-          </table>
-        </div>
+                </thead>
+                <tbody>
+                  <>
 
-        <div className={Styles.legalcopy}>
-          <p className={Styles.legal}>
-            <strong>Thank you for your patronage!</strong>
-          </p>
-          <p className={Styles.information}>
-            For Queries kindly contact us on: +2347032054367 or <span className={Styles.break}>email us @
-            queries@orisfinaEatery.com.ng</span> 
-          </p>
+                    {orderInfo?.orders.map((item, index) => {
+
+                      return (
+                        <tr className={Styles.service} key={index}>
+                          <td className={Styles.tableitem}>
+                            <p className={Styles.itemtext}> {item.meal}</p>
+                          </td>
+                          <td className={Styles.tableitem}>
+                            <p className={Styles.itemtext}>{item.quantity}</p>
+                          </td>
+                          <td className={Styles.tableitem}>
+                            <p className={Styles.itemtext}>{item.totalAmount}</p>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                    <tr className={Styles.tabletitle}>
+                      <td></td>
+                      <td className={Styles.Rate}>
+                        <h2>Total</h2>
+                      </td>
+                      <td className={Styles.payment}>
+                        <h2>{orderInfo?.totalPrice}</h2>
+                      </td>
+                    </tr>
+                  </>
+                </tbody>
+              </>
+            </table>
+          </div>
+
+          <div className={Styles.legalcopy}>
+            <p className={Styles.legal}>
+              <strong>Thank you for your patronage!</strong>
+            </p>
+            <p className={Styles.information}>
+              For Queries kindly contact us on: +2347032054367 or <span className={Styles.break}>email us @
+                queries@orisfinaEatery.com.ng</span>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    }
+  }
 }
 
 export default Index;
