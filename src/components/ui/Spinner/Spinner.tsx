2e1ab92fbdf612bb60afa77f62223726800cc81b
joshua Oyewole <joshpp2013@gmail.com>
2023-11-20 00:36:57 +0100

    added a Spinner component

--- src/components/ui/Spinner/Spinner.tsx
+++ src/components/ui/Spinner/Spinner.tsx
@@ -1,14 +1,18 @@
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import ReactLoading from "react-loading";
+//import Styles from "./_spinner.module.scss"
 
-import Styles from "./_spinner.module.scss"
 
-
-export default function Spinner() {
+export function Spinner() {
   return (
-    <div className={Styles.cover}>
-      <div className={Styles.ring}>Loading
-        <span className={Styles['loader__span']}></span>
-      </div>
-    </div>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}><ReactLoading type={"bars"} color="#333" /></div>
+  )
+}
+
+export function SpinnerButton({ title = "Processing" }) {
+  return (
+    <> <FontAwesomeIcon icon={faSpinner} spin size="lg" style={{ marginRight: "0.8rem" }} />{title}.</>
   )
 }
 
